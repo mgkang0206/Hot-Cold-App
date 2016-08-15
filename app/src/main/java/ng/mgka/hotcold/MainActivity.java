85fb38cc46bf1cdae2b347750a08ead550da3bdf
MichaelKang <mgkang0206@gmail.com>
2016-08-15 13:37:52 -0700

    onConnection added

--- app/src/main/java/ng/mgka/hotcold/MainActivity.java
+++ app/src/main/java/ng/mgka/hotcold/MainActivity.java
@@ -2,7 +2,9 @@ package ng.mgka.hotcold;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks{
@@ -17,4 +19,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     mRef = new com.firebase.client.Firebase("https://hotcold-f5f83.firebaseio.com/");
 
   }
+  @Override
+  public void onConnected(Bundle bundle){
+    Toast.makeText(this,"Connected...",Toast.LENGTH_LONG).show();
+  }
+  @Override
+  public void onConnectionSuspended(int i){
+
+  }
+  @Override
+  public void onConnectionFailed(ConnectionResult connectionResult){
+
+  }
+  
 }
